1c7918ae1ab78e9d3d1bc4ac2fb0857beba2147a
DevBar <prod@devbar.ru>
2020-12-03 18:15:57 +0300

    del dotenv, add config, add mysql2, settings webhook

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
-const { Telegraf } = require('telegraf');
-require('dotenv').config();
+const {Telegraf} = require('telegraf');
+const config = require('config');
 const Koa = require('koa');
 const koaBody = require('koa-body');
 
-const BOT_TOKEN = process.env.BOT_TOKEN || "";
-const URL = process.env.URL || "";
-const PORT = process.env.PORT || 3000;
+const BOT_TOKEN = config.get('BOT_TOKEN') || '';
+const URL = config.get('URL') || '';
+const PORT = config.get('PORT') || 3000;
 
 const bot = new Telegraf(BOT_TOKEN);
 
-// Set telegram webhook
-bot.telegram.setWebhook(`${URL}/bot`);
+bot.command('start', ({reply}) => {
+    reply('Для начала поиска необходимо ввести адрес, например, Казань Большая Красная, 100')
+});
+
+bot.telegram.setWebhook(`${URL}/my_deputy_bot`);
 
 const app = new Koa();
 app.use(koaBody());
 app.use(async (ctx, next) => {
-  if (ctx.method !== 'POST' || ctx.url !== '/bot') {
-    return next();
-  }
-  await bot.handleUpdate(ctx.request.body, ctx.response);
-  ctx.status = 200;
+    if (ctx.method !== 'POST' || ctx.url !== '/my_deputy_bot') {
+        return next();
+    }
+    await bot.handleUpdate(ctx.request.body, ctx.response);
+    ctx.status = 200;
 });
-app.use(async (ctx) => {
-  ctx.body = 'Hello World'
-})
 
 app.listen(PORT);
